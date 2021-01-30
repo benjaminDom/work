2913ae93724cef09b0782c000562a1fb3a55b90d
Benjamin Dom <benjamin.j.dominguez.o@gmail.com>
2021-01-29 22:31:59 -0600

    lets but everything

---
+++ jsnode/js/ex_5/index.js
@@ -0,0 +1,19 @@
+
+
+var FirstName="Benjamin";
+var LastName="Dominguez";
+var Age="37 ";
+var DateOfB="18/05/1983";
+var Adress="Insurgentes 2211";
+
+console.log(FirstName,LastName,Age,DateOfB,Adress);
+
+
+let x = 1;
+if (x === 1) {
+  let x = 2;
+  console.log(x);
+  // expected output: 2
+}
+console.log(x);
+// expected output: 1
---
+++ jsnode/js/index.js
@@ -0,0 +1,9 @@
+console.log(2+3/7);   
+console.log("This is a mess");   
+var x=10;
+var y=2;
+
+var z=x+y;
+
+console.log("this is z=",z);
+console.log("this is x="+x+", and  this is y="+y);
