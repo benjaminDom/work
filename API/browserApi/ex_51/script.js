cf30e21fa60991334b1ad77eba8eb9fc259f17fd
benjaminDom1 <benjamin.j.dominguez.o@gmail.com>
2021-03-18 20:15:53 -0700

    yhea

---
+++ API/browserApi/ex_51/script.js
@@ -0,0 +1,13 @@
+const user = {
+    username: 'pepe',
+    password: '12345',
+    email: 'pepe@gmail.com',
+    married: true,
+    childrens: ['maria', 'juan']
+  };
+
+  const JSONuser = JSON.stringify(user);
+console.log(user);
+console.log(JSONuser);
+
+  
